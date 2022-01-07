f8f3e42519dbda7e4ce35ebd0b37f658320a02e5
TusharDeshwall <94027033+TusharDeshwall@users.noreply.github.com>
2022-01-07 23:40:45 +0530

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,7 @@ const username = process.env.TYPEORM_USERNAME || 'postgres';
 const password = process.env.TYPEORM_PASSWORD || '9592';
 const host = process.env.TYPEORM_HOST || 'localhost';
 const port = parseInt(process.env.TYPEORM_PORT, 10) || 5432;
-const database = process.env.TYPEORM_DATABASE || 'bug_tracker';
+const database = process.env.TYPEORM_DATABASE || 'postgres';
 
 module.exports = {
   type,
